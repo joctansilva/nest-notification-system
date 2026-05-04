3fbc65786f1b4130bd13703a980296248bb83ed7
Joctan Neves <joctannsilva@gmail.com>
2026-05-03 23:56:26 -0300

    feat: :sparkles: Criaçao do producer
    Producer para publicar as tarefas na fila, a funçao é jogar o job no redis

---
+++ src/notifications/infra/queue/notification.producer.ts
@@ -0,0 +1,37 @@
+import { InjectQueue } from '@nestjs/bull';
+import { Injectable } from '@nestjs/common';
+import { Queue } from 'bullmq';
+import { Notification } from 'src/notifications/domain/notification.entity';
+
+//Nome da fila
+export const NOTIFICATION_QUEUE = 'notifications';
+
+@Injectable()
+export class NotificationProducer {
+  constructor(
+    //Injetando a instancia da fila
+    @InjectQueue(NOTIFICATION_QUEUE)
+    private readonly queue: Queue,
+  ) {}
+
+  // Recebe uma entidade de dominio e publica na fila
+  async publish(notification: Notification): Promise<void> {
+    await this.queue.add(
+      notification.event, // Nome do Job
+      {
+        recipientEmail: notification.recipientEmail,
+        payload: notification.payload,
+        createdAt: notification.createdAt,
+      },
+      {
+        attempts: 3, // Numero de tentativas se falhar
+        backoff: {
+          type: 'exponential',
+          delay: 1000, // 1 Segundo para retry
+        },
+        removeOnComplete: true, //Remove quando concluído
+        removeOnFail: false, // Mantem se falhar
+      },
+    );
+  }
+}
