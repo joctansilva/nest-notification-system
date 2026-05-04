2f795a66f889a3d2b14ebb6e601cc9175df1e978
Joctan Neves <joctannsilva@gmail.com>
2026-05-04 06:40:33 -0300

    feat: :sparkles: e instalaçao do nestjs config para leitura do process.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { BullModule } from '@nestjs/bullmq';
 import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     BullModule.forRoot({
       connection: {
         host: process.env.REDIS_HOST ?? 'localhost',
-        port: Number(process.env.REDIS_PORT) ?? 6379,
+        port: parseInt(process.env.REDIS_PORT ?? '6379', 10),
       },
     }),
     NotificationsModule,
